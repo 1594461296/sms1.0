34fa13619d00f66c8d593a34c1c905606dc025a6
1594461296 <admin@windows10.microdone.cn>
2018-07-19 08:22:42 +0800

    new

--- src/main/java/com/briup/app02/config/MybatisConfig.java
+++ src/main/java/com/briup/app02/config/MybatisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan("com.briup.app02.dao")
+
 public class MybatisConfig {
 
 }
--- src/main/java/com/briup/app02/web/controller/TestController.java
+++ src/main/java/com/briup/app02/web/controller/TestController.java
@@ -1,10 +1,10 @@
-package com.briup.app02.web.controller;
+/*package com.briup.app02.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
+*//**
  * 路由的命名空间
  * http://127.0.0.1:8080 可以访问到当前项目
  * http://127.0.0.1:8080/test 可以访问到当前项目TestController这个类
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 浏览器发送请求 --> 服务接收请求 --> 服务返回响应 --> 浏览器接收回应
  * http协议！！！
  * 
- * */
+ * *//*
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -47,3 +47,4 @@ public class TestController {
 
 
 
+*/
